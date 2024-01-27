1fe56637d7c2813adf33aec9430e59247bbba282
Dmitry Selivanov <dmitryselivanov89@yandex.ru>
2024-01-27 09:31:15 +0400

    extract css plugin

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import type {Configuration as DevServerConfiguration} from "webpack-dev-server";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 
 type Mode = 'production' | 'development'
@@ -13,6 +14,7 @@ interface EnvVariables {
 
 export default (env: EnvVariables) => {
     const isDevMode = env.mode === 'development';
+    const isProdMode = env.mode === 'production';
     const config: webpack.Configuration =
         {
             mode: env.mode ?? 'development',
@@ -26,7 +28,7 @@ export default (env: EnvVariables) => {
                     },
                     {
                         test: /\.css$/i,
-                        use: ["style-loader", "css-loader"],
+                        use: [isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader, "css-loader"],
                     },
                 ],
             },
@@ -40,7 +42,13 @@ export default (env: EnvVariables) => {
             },
             plugins: [
                 new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-                isDevMode && new webpack.ProgressPlugin()
+                isDevMode && new webpack.ProgressPlugin(),
+                isProdMode && new MiniCssExtractPlugin(
+                    {
+                        filename: "css/[name].[contenthash:8].css",
+                        chunkFilename: "css/[name].[contenthash:8]css",
+
+                    })
             ].filter(Boolean),
             devtool: isDevMode && 'inline-source-map',
             devServer: isDevMode ? {
