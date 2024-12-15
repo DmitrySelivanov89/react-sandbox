8af5a24a22fefd36ac83891961090b743c17ed61
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-12-15 22:36:33 +0400

    refactor(MediaDevices): integrate useAudioVisualize hook for audio visualization, replace setVolume with handleSetVolume, and update media device list rendering

--- src/components/mediaDevices/MediaDevices.tsx
+++ src/components/mediaDevices/MediaDevices.tsx
@@ -1,64 +1,11 @@
-import { ChangeEvent, useEffect, useRef } from "react";
+import { ChangeEvent, RefObject, useRef } from "react";
 import { useMediaDevices } from "../../hooks/useMediaDevices";
+import useAudioVisualize from "../../hooks/useAudioVisualize";
 
 const MediaDevices = () => {
-  const { mediaStream, error, loading, mediaDeviceInfo, setVolume, volume, toggleStream } = useMediaDevices();
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const analyserRef = useRef<AnalyserNode | null>(null);
-  const animationRef = useRef<number | null>(null);
-  const audiCtxRef = useRef<AudioContext | null>(null);
-
-  useEffect(() => {
-    if (mediaStream) {
-      audiCtxRef.current = new AudioContext();
-      const source = audiCtxRef.current.createMediaStreamSource(mediaStream);
-      analyserRef.current = audiCtxRef.current.createAnalyser();
-      source.connect(analyserRef.current);
-      analyserRef.current.connect(audiCtxRef.current.destination);
-
-      const ctx = canvasRef.current?.getContext("2d");
-      const bufferLength = analyserRef.current.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-
-      const draw = () => {
-        animationRef.current = requestAnimationFrame(draw);
-        analyserRef.current?.getByteFrequencyData(dataArray);
-
-        if (ctx && canvasRef.current) {
-          ctx.fillStyle = "rgba(200, 200, 200, 0.2)";
-          ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-          const barWidth = (canvasRef.current.width / bufferLength) * 2.5;
-          let barHeight;
-          let x = 0;
-
-          for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            ctx.fillStyle = "rgb(" + (barHeight + 100) + ",50,50)";
-            ctx.fillRect(x, canvasRef.current.height - barHeight / 2, barWidth, barHeight / 2);
-            x += barWidth + 1;
-          }
-        }
-      };
-      draw();
-    }
-    return () => {
-      if (animationRef.current) {
-        cancelAnimationFrame(animationRef.current);
-        animationRef.current = null;
-      }
-      if (mediaStream) {
-        mediaStream.getTracks().forEach(track => track.stop());
-      }
-      if (analyserRef.current) {
-        analyserRef.current?.disconnect();
-        analyserRef.current = null;
-      }
-      if (audiCtxRef.current) {
-        audiCtxRef.current.close();
-        audiCtxRef.current = null;
-      }
-    };
-  }, [mediaStream]);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const { mediaStream, error, loading, mediaDeviceInfo, handleSetVolume, volume, toggleStream } = useMediaDevices();
+  useAudioVisualize(mediaStream, canvasRef);
 
   if (loading) {
     return <div>Загрузка...</div>;
@@ -69,7 +16,7 @@ const MediaDevices = () => {
   }
 
   const handleVolumeChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setVolume(parseFloat(e.target.value));
+    handleSetVolume(parseFloat(e.target.value));
   };
 
   return (<>
@@ -89,7 +36,7 @@ const MediaDevices = () => {
     </div>
     <canvas ref={canvasRef} width={300} height={100} />
     <ul>
-      {mediaDeviceInfo.map((device) => <li key={device.deviceId}>{device.label} {device.kind}</li>)}
+      {mediaDeviceInfo.map((device, i) => <li key={i}>{`${device.label} ${device.kind}`}</li>)}
     </ul>
   </>);
 };
